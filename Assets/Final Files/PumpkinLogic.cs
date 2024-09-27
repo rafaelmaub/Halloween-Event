e8d7e933821805b5be84c71ea449fb5be57c7aee
rafaelmaub <deepcavestudios@gmail.com>
2024-09-27 12:04:58 -0400

    Pumpkin being collected
    -Spread them around the map
    -Teach about prefabs by modifying just the prefab
    -Add the script
    -Make it rotate
    -Add On Trigger Enter

--- Assets/Final Files/PumpkinLogic.cs
+++ Assets/Final Files/PumpkinLogic.cs
@@ -7,8 +7,16 @@ public class PumpkinLogic : MonoBehaviour
     private void Update()
     {
         //Rotate
+        transform.Rotate(0, 30 * Time.deltaTime, 0);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
     //On Trigger Enter
     //Tell GameManager for pumpkin collected
     //Destroy this game object
