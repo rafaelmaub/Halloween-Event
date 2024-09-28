3b9722cf89bf12431bb16c6749eb1c19d77075ea
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2024-09-28 11:41:04 -0400

    Spawner Prefab and Script
    -Make enemy Translate with Vector3.forward
    -Create a script that creates enemy prefabs every few seconds
    -Place spawners and explain about rotation
    -(Optional) Make prefab out of spawners

--- Assets/Final Files/Enemy.cs
+++ Assets/Final Files/Enemy.cs
@@ -9,13 +9,13 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 20f);
     }
 
     // Update is called once per frame
     void Update()
     {
         //Moving enemy
-        transform.Translate(transform.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
